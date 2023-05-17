b4bc1ed4af6060a040e3be3ac53d24416ffb29fd
thekingvice <villarrealvicente92@gmail.com>
2023-05-16 22:07:09 -0700

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,82 @@
+import { useState } from "react";
 import "./App.css";
 import Map from "./Map";
 
 function App() {
+  const [ipAddress, setIpAddress] = useState("");
+  const [coordinates, setCoordinates] = useState([51.505, -0.09]);
+
+  //get ip address, set ip address
+  //search for more info using ip address
+  //pass down courdinates to map and use it in map element
+
+  //create search bar with default value of ip address
+  //on submit check if valid ip address, if valid
+
+  // fetch('https://api.ipify.org?format=json')
+  // .then(response => response.json())
+  // .then(data => console.log('My public IP address is:', data.ip))
+  // .catch(error => console.error('Error fetching IP:', error));
+
+  const handleIP = () => {
+    fetch("https://api.ipify.org?format=json")
+      .then((response) => response.json())
+      .then((data) => setIpAddress(data.ip))
+      .catch((error) => console.error("Error fetching IP:", error));
+  };
+
+  const handleCoordinates = () => {
+    let coordinatesList = [];
+    let addressData = {};
+    const apiKey = "at_UtjCdoDfEBMQHwZjIbCvqsWaL1txO";
+    const apiUrl = `https://geo.ipify.org/api/v1?apiKey=${apiKey}&ipAddress=${ipAddress}`;
+
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        coordinatesList = [data.location.lat, data.location.lng];
+        console.log(coordinatesList);
+        setCoordinates(coordinatesList);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  };
+
   return (
     <div className="App">
-      <Map></Map>
+      <button onClick={handleIP}>Handle IP:{ipAddress}</button>
+      <button onClick={handleCoordinates}>Handle Coordinates</button>
+      <Map coordinates={coordinates}></Map>
     </div>
   );
 }
 
 export default App;
 
-//hide api keys in .env file
-//use ipify to find coordinates of ip address
-//use google maps api to map coordinates on a map
+// {
+//   "ip": "97.120.46.202",
+//   "location": {
+//       "country": "US",
+//       "region": "Oregon",
+//       "city": "Portland",
+//       "lat": 45.52345,
+//       "lng": -122.67621,
+//       "postalCode": "97201",
+//       "timezone": "-07:00",
+//       "geonameId": 5746545
+//   },
+//   "as": {
+//       "asn": 209,
+//       "name": "CENTURYLINK-US-LEGACY-QWEST",
+//       "route": "97.120.0.0/16",
+//       "domain": "http://www.lumen.com",
+//       "type": "NSP"
+//   },
+//   "isp": "CenturyLink",
+//   "proxy": {
+//       "proxy": false,
+//       "vpn": false,
+//       "tor": false
+//   }
+// }
--- src/Map.jsx
+++ src/Map.jsx
@@ -1,24 +1,36 @@
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, useMap } from "react-leaflet";
 import { Icon } from "leaflet";
 import "leaflet/dist/leaflet.css";
 
-export default function Map() {
+export default function Map(props) {
+  const { coordinates } = props;
+
   const customIcon = new Icon({
     iconUrl: "/icon-location.svg",
     iconSize: ["46px", "56px"],
   });
+
+  const ChangeMap = () => {
+    const randcoord = [45.52345, -122.67621];
+    const map = useMap();
+    map.flyTo(randcoord);
+  };
+
   return (
-    <MapContainer
-      style={{ height: "50rem" }}
-      center={[51.505, -0.09]}
-      zoom={15}
-      scrollWheelZoom={false}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker icon={customIcon} position={[51.505, -0.09]}></Marker>
-    </MapContainer>
+    <>
+      <button onClick={ChangeMap}>change map view</button>
+      <MapContainer
+        style={{ height: "50rem" }}
+        center={coordinates}
+        zoom={12}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker icon={customIcon} position={coordinates}></Marker>
+      </MapContainer>
+    </>
   );
 }
