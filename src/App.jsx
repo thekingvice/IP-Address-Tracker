fb4ce8ecd10d7cce3b9b52f5b30ed9a02779bfb7
thekingvice <villarrealvicente92@gmail.com>
2023-05-22 22:08:21 -0700

    data is displaying correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,57 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Map from "./Map";
 
 function App() {
-  const [ipAddress, setIpAddress] = useState("");
   const [coordinates, setCoordinates] = useState([51.505, -0.09]);
+  const [ipAddress, setIpAddress] = useState("");
+  const [ipData, setIpData] = useState({
+    location: {
+      region: "",
+      city: "",
 
-  //create search bar with default value of ip address
-  //on submit check if valid ip address, if valid
+      postalCode: "",
+      timezone: "",
+    },
 
-  // fetch('https://api.ipify.org?format=json')
-  // .then(response => response.json())
-  // .then(data => console.log('My public IP address is:', data.ip))
-  // .catch(error => console.error('Error fetching IP:', error));
+    isp: "",
+  });
+
+  function handleData() {
+    //set ip address to input value
+    let coordinatesList = [];
+    let ipDataObj = {
+      location: {
+        region: "",
+        city: "",
+
+        postalCode: "",
+        timezone: "",
+      },
+
+      isp: "",
+    };
+    const apiKey = "at_UtjCdoDfEBMQHwZjIbCvqsWaL1txO";
+    const apiUrl = `https://geo.ipify.org/api/v1?apiKey=${apiKey}&ipAddress=${ipAddress}`;
+
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        coordinatesList = [data.location.lat, data.location.lng];
+        ipDataObj.location.region = data.location.region;
+        ipDataObj.location.city = data.location.city;
+        ipDataObj.location.postalCode = data.location.postalCode;
+        ipDataObj.location.timezone = data.location.timezone;
+        ipDataObj.isp = data.isp;
+        setIpData(ipDataObj);
+        console.log(ipDataObj);
+        setCoordinates(coordinatesList);
+        console.log(coordinatesList);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }
 
   const handleIP = () => {
     fetch("https://api.ipify.org?format=json")
@@ -21,34 +60,95 @@ function App() {
       .catch((error) => console.error("Error fetching IP:", error));
   };
 
-  const handleCoordinates = () => {
-    let coordinatesList = [];
-    let addressData = {};
-    const apiKey = "at_UtjCdoDfEBMQHwZjIbCvqsWaL1txO";
-    const apiUrl = `https://geo.ipify.org/api/v1?apiKey=${apiKey}&ipAddress=${ipAddress}`;
+  useEffect(() => {
+    handleIP();
+  }, [ipAddress]);
 
-    fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data) => {
-        coordinatesList = [data.location.lat, data.location.lng];
-        console.log(coordinatesList);
-        setCoordinates(coordinatesList);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
-  };
+  function validateIPAddress(ipAddress) {
+    // Regular expression pattern for IPv4 address
+    const ipv4Pattern = /^(\d{1,3}\.){3}\d{1,3}$/;
+
+    // Regular expression pattern for IPv6 address
+    const ipv6Pattern = /^([0-9A-Fa-f]{1,4}:){7}[0-9A-Fa-f]{1,4}$/;
+
+    if (ipv4Pattern.test(ipAddress)) {
+      // Valid IPv4 address
+      console.log(true);
+      return true;
+    } else if (ipv6Pattern.test(ipAddress)) {
+      // Valid IPv6 address
+      console.log(true);
+      return true;
+    } else {
+      // Invalid IP address
+      console.log(ipAddress);
+      return false;
+    }
+  }
 
   return (
     <div className="App">
-      <button onClick={handleIP}>Handle IP:{ipAddress}</button>
-      <button onClick={handleCoordinates}>Handle Coordinates</button>
+      <section className="App__search">
+        <h1 className="App__header">IP Address Tracker</h1>
+        <div className="App__searchbar-wrapper">
+          <input
+            className="App__searchbar"
+            type="text"
+            name="search"
+            placeholder="Search for an IP Address"
+          />
+          <button
+            onClick={() => {
+              validateIPAddress(ipAddress);
+              handleData();
+            }}
+          >
+            Submit
+          </button>
+        </div>
+        <section className="App__details">
+          <div className="App__details-section">
+            <p className="App__details-p">IP ADDRESS</p>
+            <h1 className="App__details-h1">{ipAddress}</h1>
+          </div>
+          <div className="App__details-section">
+            <p className="App__details-p">LOCATION</p>
+            <h1 className="App__details-h1">
+              {ipData.location.city}, {ipData.location.region}{" "}
+              {ipData.location.postalCode}
+            </h1>
+          </div>
+          <div className="App__details-section">
+            <p className="App__details-p">TIMEZONE</p>
+            <h1 className="App__details-h1">{ipData.location.timezone}</h1>
+          </div>
+          <div className="App__details-section">
+            <p className="App__details-p">ISP</p>
+            <h1 className="App__details-h1">{ipData.isp}</h1>
+          </div>
+        </section>
+      </section>
+
       <Map coordinates={coordinates} setCoordinates={setCoordinates}></Map>
     </div>
   );
 }
 
 export default App;
+
+// --project layout--
+
+// fetch current ip
+// use ip to fetch locational data
+// use locational data to set map to current location
+// display locational data
+
+// when search button is clicked
+// validate ip
+// if ip is valid
+// > use ip to change map and data display
+// else
+// > display error
 
 // {
 //   "ip": "97.120.46.202",
--- src/Map.jsx
+++ src/Map.jsx
@@ -1,45 +1,3 @@
-// import { MapContainer, TileLayer, Marker } from "react-leaflet";
-// import { Icon, setView } from "leaflet";
-// import "leaflet/dist/leaflet.css";
-// import { useCallback, useState } from "react";
-
-// export default function Map(props) {
-//   const { coordinates, setCoordinates } = props;
-
-//   const testCoordinates = [22, 22];
-
-//   const customIcon = new Icon({
-//     iconUrl: "/icon-location.svg",
-//     iconSize: ["46px", "56px"],
-//   });
-
-//   const [map, setMap] = useState(null);
-
-//   const onClickSet = useCallback(() => {
-//     setCoordinates(testCoordinates);
-//     map.setView(testCoordinates, 12);
-//   }, [map]);
-
-//   return (
-//     <>
-//       <button onClick={onClickSet}>change map view</button>
-//       <MapContainer
-//         style={{ height: "50rem" }}
-//         center={coordinates}
-//         zoom={12}
-//         scrollWheelZoom={false}
-//         whenCreated={setMap}
-//       >
-//         <TileLayer
-//           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//         />
-//         <Marker icon={customIcon} position={coordinates}></Marker>
-//       </MapContainer>
-//     </>
-//   );
-// }
-
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import { Icon } from "leaflet";
 import "leaflet/dist/leaflet.css";
@@ -64,14 +22,15 @@ export default function Map(props) {
 
   const [map, setMap] = useState(null);
 
-  const onClickSet = useCallback(() => {
-    setCoordinates(testCoordinates);
-    map.setView(testCoordinates, 12);
-  }, [map]);
+  // const onClickSet = useCallback(() => {
+  //   setCoordinates(testCoordinates);
+  //   map.setView(testCoordinates, 12);
+  // }, [map]);
 
   return (
     <>
-      <button onClick={onClickSet}>change map view</button>
+      {/* <button onClick={onClickSet}>{`<button>SetMapView();</button>`}</button> */}
+
       <MapContainer
         style={{ height: "50rem" }}
         center={coordinates}
